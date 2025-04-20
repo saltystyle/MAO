156580070161cf791cb7f560ddfcf527cc288c50
saltystyle <94041050+saltystyle@users.noreply.github.com>
2025-04-20 21:47:44 +0900

    Update qlook1.py

--- mao_epl/qlook1.py
+++ mao_epl/qlook1.py
@@ -38,7 +38,7 @@ from docopt import docopt
 from .reader1 import  get_cal_spectrum, generate_patterned, get_nth_spectrum_in_range, convert_spectrum_to_epl, get_n_from_current_time
 
 import re
-from typing import Callable, Pattern
+from typing import Pattern
 import csv 
 import datetime
 
