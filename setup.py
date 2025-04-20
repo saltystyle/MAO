8e23ddf3e2d0c827fb9a21ca1cbb2b4ae0ccb79b
saltystyle <94041050+saltystyle@users.noreply.github.com>
2025-04-20 21:11:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mao_epl',  # パッケージ名（pip listで表示される）
-    version="0.1.0",  # バージョン
+    version="0.1.1",  # バージョン
     description="EPL",  # 説明
     author='Shion Takeno',  # 作者名
     packages=find_packages(),  # 使うモジュール一覧を指定する
